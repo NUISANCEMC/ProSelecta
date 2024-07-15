dd41087625ba57fa168e356be9b9bfbf9300b6e5
Luke Pickering <luketpickering@gmail.com>
2024-07-15 12:34:01 +0000

    slightly improves prettifying of generated source code

--- app/ProSelectaBuild.py
+++ app/ProSelectaBuild.py
@@ -13,9 +13,7 @@ if len(sys.argv) < 3:
 with open(sys.argv[1]) as manifest:
   data = yaml.safe_load(manifest)
 
-snippet_files = []
-project_funcs = []
-select_funcs = []
+snippet_files = {}
 
 for analysis in data:
   snippet = analysis["snippet"]
@@ -26,34 +24,40 @@ for analysis in data:
   print(f"  checking select functions")
   funcs = analysis["functions"]
 
+  snippet_files[snippet] = { "select": [], "project": [] }
+
   for sf in funcs["select"]:
     print(f"    fetching symbol for {sf}")
     if pps.select.get(sf):
-      select_funcs.append(sf)
+      snippet_files[snippet]["select"].append(sf)
     else:
       raise RuntimeError(f"Failed to read selection function: {sf}")
-
 
   print(f"  checking project functions")
   for sf in funcs["project"]:
     print(f"    fetching symbol for {sf}")
     if pps.project.get(sf):
-      project_funcs.append(sf)
+      snippet_files[snippet]["project"].append(sf)
     else:
       raise RuntimeError(f"Failed to read projection function: {sf}")
-
-  snippet_files.append(snippet)
 
 outproj = sys.argv[2]
 
 with open(f"{outproj}.h",'w') as outputh:
-  outputh.write('#include "ProSelecta/env.h"\n')
+  outputh.write('#include "ProSelecta/env.h"\n\n')
 
-  for sf in select_funcs:
-    outputh.write(f'int {sf}(HepMC3::GenEvent const&);\n')
+  for sn in snippet_files.keys():
+    outputh.write(f'//START -- Prototypes for {sn}\n\n')
+    outputh.write(f'//START -- select functions\n')
+    for sf in snippet_files[sn]["select"]:
+      outputh.write(f'int {sf}(HepMC3::GenEvent const&);\n')
 
-  for sf in project_funcs:
-    outputh.write(f'double {sf}(HepMC3::GenEvent const&);\n')
+    outputh.write(f'//END   -- select functions\n\n')
+    outputh.write(f'//START -- project functions\n')
+    for sf in snippet_files[sn]["project"]:
+      outputh.write(f'double {sf}(HepMC3::GenEvent const&);\n')
+    outputh.write(f'//END   -- project functions\n\n')
+    outputh.write(f'//END   -- Prototypes for {sn}\n\n')
 
 cproc = subprocess.run(["HepMC3-config", "--includedir"], capture_output=True)
 if cproc.returncode != 0:
@@ -70,13 +74,13 @@ hepmc3lib = cproc.stdout.decode("utf-8").strip()
 ProSelecta_ROOT = os.environ["ProSelecta_ROOT"]
 
 with open(f"{outproj}.cxx",'w') as outputimpl:
-  outputimpl.write(f'#include "{outproj}.h"\n')
-  for sn in snippet_files:
+  outputimpl.write(f'#include "{outproj}.h"\n\n')
+  for sn in snippet_files.keys():
     with open(sn,'r') as snf:
       outputimpl.write(f"//START -- imported from {sn}\n")
       for line in snf:
         outputimpl.write(line)
-      outputimpl.write(f"//END -- imported from {sn}\n")
+      outputimpl.write(f"\n//END -- imported from {sn}\n\n")
 
 ccmd = ["g++", "-std=c++17", "-O3", f"-I{hepmc3inc}", f"-L{hepmc3lib}", f"-I{ProSelecta_ROOT}/include", "-shared", "-fPIC", "-o", f"{outproj}.so", f"{outproj}.cxx"]
 
